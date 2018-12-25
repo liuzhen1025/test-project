6e8b2e88381aa717b62d33f03a3761a1b02332f0
刘朕 <liuzhen@gennlife.com>
2018-12-25 15:38:16 +0800

    测试新功能

--- websocket/src/main/resources/static/webSocket.js
+++ websocket/src/main/resources/static/webSocket.js
@@ -26,8 +26,7 @@ function connect(){
     var userId="1000002";
     var socket = new SockJS('http://127.0.0.1:8080/nmpSocketWeb'); //连接SockJS的endpoint名称为"endpointOyzc"
     stompClient = Stomp.over(socket);//使用STMOP子协议的WebSocket客户端
-    stompClient = Stomp.over(socket);//使用STMOP子协议的WebSocket客户端
-    stompClient.connect({},function(frame){//连接WebSocket服务端
+    stompClient.connect({"oauth":userId},function(frame){//连接WebSocket服务端
         console.log('Connected:' + frame);
         //通过stompClient.subscribe订阅/topic/getResponse 目标(destination)发送的消息
         stompClient.subscribe('/user/' + userId + '/user/getResponse',function(response){
